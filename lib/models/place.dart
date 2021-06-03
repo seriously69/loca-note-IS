1115fefb4f6c05552175e92c5c5760751e2d79c8
seriously69 <72123077+seriously69@users.noreply.github.com>
2021-06-03 13:41:13 +0400

    Add files via upload

---
+++ lib/models/place.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+  const PlaceLocation({
+    @required this.latitude,
+    @required this.longitude,
+    this.address,
+  });
+}
+
+class Place {
+  final String id;
+  final String title;
+  final String description;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.location,
+    @required this.image,
+  });
+}
